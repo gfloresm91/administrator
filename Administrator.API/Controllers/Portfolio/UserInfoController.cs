34477d42067a711e43cdfd2337ae078a48e2d41c
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2022-07-02 18:53:30 -0400

    - Implement MediatR and Behaviours - Inject dependencies into the application project - Create UserInfo controller

---
+++ Administrator.API/Controllers/Portfolio/UserInfoController.cs
@@ -0,0 +1,55 @@
+﻿using Administrator.Application.Features.Portfolio.UsersInfo.Commands.CreateUserInfo;
+using Administrator.Application.Features.Portfolio.UsersInfo.Commands.DeleteUserInfo;
+using Administrator.Application.Features.Portfolio.UsersInfo.Commands.UpdateUserInfo;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Administrator.API.Controllers.Portfolio
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UserInfoController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserInfoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost(Name = "CreateUserInfo")]
+        [ProducesResponseType((int) HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CreateUserInfo([FromBody] CreateUserInfoCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPut(Name = "UpdateUserInfo")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateUserInfo([FromBody] UpdateUserInfoCommand command)
+        {
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteUserInfo")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteUserInfo(int id)
+        {
+            var command = new DeleteUserInfoCommand
+            {
+                Id = id
+            };
+
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
